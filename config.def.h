1548063f241b6285c29f647d77654695b7ea3bf5
Mr.X <abas93931@gmail.com>
2023-08-17 02:39:45 +0300

    initial commit

--- config.def.h
+++ config.def.h
@@ -22,4 +22,4 @@ static const char * message = "The quieter you become, the more you are able to 
 static const char * text_color = "#ffffff";
 
 /* text size (must be a valid size) */
-static const char * font_name = "6x13";
+static const char * font_name = "fixed";
